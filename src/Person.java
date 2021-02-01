eaf9784a10111131692eefe0ed0b2802b02fadff
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-01-31 23:49:02 -0600

    continuing object exercises

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,19 @@
+
 public class Person {
+    public static void main(String[] args) {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public void sayHello() {
+
+    }
 }
+
