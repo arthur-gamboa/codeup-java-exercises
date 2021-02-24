dad73a208ffe3ffded2035642bba5949d9823841
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-24 00:20:45 -0600

    adjusting rectangle constructor

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -11,12 +11,12 @@ public class ShapesTest {
         System.out.println("myShape.getArea() = " + myShape.getArea());
         System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
 
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println(box1.getArea());
-        System.out.println(box1.getPerimeter());
-
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getArea());
-        System.out.println(box2.getPerimeter());
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println(box1.getArea());
+//        System.out.println(box1.getPerimeter());
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getArea());
+//        System.out.println(box2.getPerimeter());
     }
 }
